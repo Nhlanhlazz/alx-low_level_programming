434871f82a63a418d15720b19e779f8b3260350b
Nhlanhlazz <nhlanhlazz@gmail.com>
2024-06-25 14:21:18 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Nonhlanhla Maisha
+ * Description: Create a header file that brings functions in the 
+ * 		nested loops directory
+ */
+
+
+int _putchar(char c);
+#endif
