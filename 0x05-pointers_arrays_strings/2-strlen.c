679f3e608b9dd3e59069de9093a9798e22f88342
Nhlanhlazz <nhlanhlazz@gmail.com>
2024-07-02 05:34:36 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,15 @@
 #include "main.h"
 /**
  * _strlen - returns the length of a string
- * @s: string
- * Return: length
+ * @s: string to be counted
+ * Return: length of the string
  */
-int_strlen(char *s)
+int _strlen(char *s)
 {
-	int longi = 0
+	int i;
+	int count = 0;
 
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
-
-	return (longi);
+	for (i = 0; s[i] != '\0' ; i++)
+		count++;
+	return (count);
 }
