fd3f38391979a634c2eb020651f32626eb49d09e
Nhlanhlazz <nhlanhlazz@gmail.com>
2024-07-13 12:57:09 -0700

    updated

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -12,15 +12,15 @@ unsigned int _strspn(char *s, char *accept)
 
 	while (*s)
 	{
-		for (index = 0, accept[index]; index++;)
+		for (index = 0; accept[index]; index++)
 		{
 			if (*s == accept[index])
 			{
 				bytes++;
 				break;
 			}
-			else if (accept[index + 1] == '\0')
-				return (bytes);
+			if (accept[index] == '\0')
+				break;
 		}
 		s++;
 	}
